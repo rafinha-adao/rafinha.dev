927c52ec8e2a409c1f548c928aea492a61aae987
Rafael Adão <raphinha.oliveira11@gmail.com>
2026-01-03 13:52:56 -0300

    feat: update buymeacoffee widget color

--- app/Views/pages/noshorts.php
+++ app/Views/pages/noshorts.php
@@ -109,7 +109,7 @@
             </p>
         </section>
     </main>
-    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="noshorts" data-description="Support me on Buy me a coffee!" data-message="" data-color="#FF5F5F" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
+    <script data-name="BMC-Widget" data-cfasync="false" src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js" data-id="noshorts" data-description="Support me on Buy me a coffee!" data-message="" data-color="#ED1C24" data-position="Right" data-x_margin="18" data-y_margin="18"></script>
 </body>
 
 </html>
